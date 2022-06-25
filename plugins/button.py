786468265fee1725e7c1573d76d11d5dc8b6d79b
Ridhoobot <106302889+Ridhoobot@users.noreply.github.com>
2022-06-26 00:27:29 +0800

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -44,7 +44,7 @@ def start_button(client):
             ],
             [
                 InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
-                InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink2),
+                InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink2),
             ],
             [InlineKeyboardButton(text="• ᴛᴜᴛᴜᴘ •", callback_data="close")],
         ]
