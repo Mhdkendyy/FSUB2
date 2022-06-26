d8d096d59b74d4d8cf9bce7dd148a54369df8035
Ridho X Apo Nnetawin <106302889+Ridhoobot@users.noreply.github.com>
2022-06-26 21:50:09 +0800

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -92,7 +92,7 @@ def fsub_button(client, message):
         buttons = [
             [
                 InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
-                InlineKeyboardButton(text="ᴊᴏɪɴ ɢʀᴏᴜᴘ", url=client.invitelink2),
+                InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink2),
             ],
         ]
         try:
